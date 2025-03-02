c551ccdca668719ae7f0397ad2aa34ec0e89f916
Kirill <kpaskevic39@gmail.com>
2025-03-02 18:25:33 +0200

    feat/Add app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter App.");
-
+        System.out.println("App for measures converting.");
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
         System.out.println("Result is" + pounds + " pounds.");
