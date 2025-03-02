81a6a646d5baffc251bd7c6ed423e8b0169441ae
Kirill <kpaskevic39@gmail.com>
2025-03-02 18:23:44 +0200

    feat/Add pounds to kgs convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,12 +8,18 @@ public class Main {
         System.out.println("Converter App.");
 
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Result is" + pounds + " pounds.");
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is" + pounds + " pounds and " + kilos + " kgs.");
     }
 
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 
 }
